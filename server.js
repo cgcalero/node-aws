622f9e63d00f927f17182805696c2d4bcb4d3c06
Christiam G. Calero Solano <ccalero@onpe.gob.pe>
2021-03-28 00:26:04 -0500

    update port 8080

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const express = require('express')
 const app = express()
 
 app.get("/", (req, res)=>{
-    res.send("<h1>Hello World with Express with HTML!...</h1>")
+    res.send("<h1>Hello World. Express with HTML on port 8080!...</h1>")
 })
 
-app.listen(3000)
+app.listen(8080)
 
-console.log('Server listen on port 3000');
+console.log('Server listen on port 8080');
