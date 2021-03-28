6102a99da699c416eb221e073598bda3d8823ebc
Christiam G. Calero Solano <ccalero@onpe.gob.pe>
2021-03-28 00:23:13 -0500

    update commit

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 
 app.get("/", (req, res)=>{
-    res.send("Hello World with Express!...")
+    res.send("<h1>Hello World with Express with HTML!...</h1>")
 })
 
 app.listen(3000)
